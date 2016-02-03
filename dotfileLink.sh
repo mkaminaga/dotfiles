63ab2157b738e922855119ed117bc3b0f5165dcc
mkaminaga <mkaminaga2.718@gmai.com>
2016-02-03 23:41:48 +0900

    .vimrc separated into 2 files, lighter one in use.

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -5,6 +5,8 @@ ln -s -f "${HOME}/dotfiles/.bash_profile"  "${HOME}/.bash_profile"
 ln -s -f "${HOME}/dotfiles/.screenrc"      "${HOME}/.screenrc"
 ln -s -f "${HOME}/dotfiles/.vim"           "${HOME}/.vim"
 ln -s -f "${HOME}/dotfiles/.vimrc"         "${HOME}/.vimrc"
+ln -s -f "${HOME}/dotfiles/.vimrc_HEAVY"   "${HOME}/.vimrc_HEAVY"
+ln -s -f "${HOME}/dotfiles/.vimrc_LIGHT"   "${HOME}/.vimrc_LIGHT"
 ln -s -f "${HOME}/dotfiles/.virc"          "${HOME}/.virc"
 
 #code under this has no effect to mintty.
