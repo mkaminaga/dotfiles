3dc61fc716233a934522a0893416de175af746c8
mkaminaga <mkaminaga2.718@gmail.com>
2016-03-24 14:26:35 +0900

    Path collected

--- generateDirLinks.sh
+++ generateDirLinks.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-SVN_LOCAL_PATH=/cygdrive/d/Documents/ITF-2
+SVN_LOCAL_PATH=/cygdrive/c/Users/Mamoru/Documents/ITF-2
 
-#homeから主要フォルダにアクセス
-ln -s -f /cygdrive/c/Users/Mamoru/Downloads			~/Downloads;
-ln -s -f /cygdrive/c/Users/Mamoru/Desktop			~/Desktop;
-ln -s -f /cygdrive/c/Users/Mamoru/Documents			~/Documents;
-ln -s -f /cygdrive/c/Users/Mamoru/Pictures			~/Pictures;
-ln -s -f /cygdrive/c/Users/Mamoru/Videos			~/Videos;
-ln -s -f /cygdrive/c/Users/Mamoru/Music				~/Music;
+#Windows
+ln -s -f /cygdrive/c/Users/Mamoru/Downloads	~/Downloads;
+ln -s -f /cygdrive/c/Users/Mamoru/Desktop	~/Desktop;
+ln -s -f /cygdrive/c/Users/Mamoru/Documents	~/Documents;
+ln -s -f /cygdrive/c/Users/Mamoru/Pictures	~/Pictures;
+ln -s -f /cygdrive/c/Users/Mamoru/Videos	~/Videos;
+ln -s -f /cygdrive/c/Users/Mamoru/Music		~/Music;
 
-#よく使うSVNのパスへのショートカット
+#SVN
+ln -s -f ${SVN_LOCAL_PATH}							~/ITF-2;
 ln -s -f ${SVN_LOCAL_PATH}/code_share/"PIC(Pow)"	~/Powpic;
 ln -s -f ${SVN_LOCAL_PATH}/code_share/"PIC(Com)"	~/Compic;
 ln -s -f ${SVN_LOCAL_PATH}/"C&Dh"/コマンド処理		~/Command;
