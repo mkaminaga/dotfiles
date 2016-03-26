93e1b8ea27da28ec631cc584e091c91fbbb019ad
mkaminaga <mkaminaga2.718@gmail.com>
2016-03-26 13:23:40 +0900

    link gen changed

--- generateDirLinks.sh
+++ generateDirLinks.sh
@@ -12,9 +12,4 @@ ln -s -f /cygdrive/c/Users/Mamoru/Music		~/Music;
 
 #SVN
 ln -s -f ${SVN_LOCAL_PATH}							~/ITF-2;
-ln -s -f ${SVN_LOCAL_PATH}/code_share/"PIC(Pow)"	~/Powpic;
-ln -s -f ${SVN_LOCAL_PATH}/code_share/"PIC(Com)"	~/Compic;
-ln -s -f ${SVN_LOCAL_PATH}/"C&Dh"/コマンド処理		~/Command;
-ln -s -f ${SVN_LOCAL_PATH}/PCB/CDH/PFM				~/FM;
-ln -s -f ${SVN_LOCAL_PATH}/PCB/CDH/EM				~/EM;
 ln -s -f ${SVN_LOCAL_PATH}/"C&Dh"					~/CDH;
