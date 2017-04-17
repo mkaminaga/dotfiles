517893e25fcd040e7a1fb742a206a202277c103c
mkaminaga <mkaminaga2.718@gmail.com>
2017-04-17 12:22:07 +0900

    update dir

--- generateDirLinks.sh
+++ generateDirLinks.sh
@@ -1,34 +1,30 @@
 #!/bin/sh
 
 #Windows
-ln -s -f /cygdrive/c/Users/Mamoru/Downloads	~/Downloads;
-ln -s -f /cygdrive/c/Users/Mamoru/Desktop	~/Desktop;
-ln -s -f /cygdrive/c/Users/Mamoru/Documents	~/Documents;
-ln -s -f /cygdrive/c/Users/Mamoru/Pictures	~/Pictures;
-ln -s -f /cygdrive/c/Users/Mamoru/Videos	~/Videos;
-ln -s -f /cygdrive/c/Users/Mamoru/Music		~/Music;
+ln -s -f /cygdrive/c/Users/Mamoru/Downloads ~/Downloads;
+ln -s -f /cygdrive/c/Users/Mamoru/Desktop   ~/Desktop;
+ln -s -f /cygdrive/c/Users/Mamoru/Documents ~/Documents;
+ln -s -f /cygdrive/c/Users/Mamoru/Pictures  ~/Pictures;
+ln -s -f /cygdrive/c/Users/Mamoru/Videos    ~/Videos;
+ln -s -f /cygdrive/c/Users/Mamoru/Music     ~/Music;
 
 #SVN
 SVN_LOCAL_PATH=/cygdrive/c/Users/Mamoru/Documents/ITF-2
-ln -s -f ${SVN_LOCAL_PATH}							~/ITF-2;
-ln -s -f ${SVN_LOCAL_PATH}/"C&Dh"					~/CDH;
-ln -s -f ${SVN_LOCAL_PATH}/"地上局"					~/groundStation;
+ln -s -f ${SVN_LOCAL_PATH}                  ~/ITF-2;
+ln -s -f ${SVN_LOCAL_PATH}/"C&Dh"           ~/CDH;
+ln -s -f ${SVN_LOCAL_PATH}/"地上局"         ~/groundStation;
 
-#Google Drive
-ln -s -f /cygdrive/c/Users/Mamoru/"Google ドライブ"	~/gdrive
-
-#Univ of tsukuba
-ln -s -f /cygdrive/c/Users/Mamoru/Documents/ut_lectures/エネルギー工学応用実験	~/reports
+#Univ
+ln -s -f /cygdrive/c/Users/Mamoru/Documents/ut_lectures/Lab ~/Lab
 
 #yui project
-ln -s -f /cygdrive/c/Users/Mamoru/Documents/YUI	~/yui
+ln -s -f /cygdrive/c/Users/Mamoru/Documents/YUI ~/yui
 
 #programming
-ln -s -f /cygdrive/c/projects	~/projects
+ln -s -f /cygdrive/c/projects ~/projects
 
 # gimp 2.0
 ln -s -f /cygdrive/c/cygwin64/home/Mamoru/.gimp-2.8/palettes ~/gimpPalettes
 
 #vim include completion dir setting
-ln -s -f /cygdrive/c/DxLib_VC/Lib	~/.vim/dir/dxlib
-ln -s -f /cygdrive/c/"Program Files (x86)"/"Microsoft DirectX SDK (June 2010)"/Include	~/.vim/dir/directx
+ln -s -f /cygdrive/c/"Program Files (x86)"/"Microsoft DirectX SDK (June 2010)"/Include  ~/.vim/dir/directx
