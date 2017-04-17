2c8766405cfbe42cc10f537b37740f79c83c208d
mkaminaga <mkaminaga2.718@gmail.com>
2017-04-17 12:42:06 +0900

    links updated

--- generateDirLinks.sh
+++ generateDirLinks.sh
@@ -7,7 +7,7 @@ ln -s -f /cygdrive/c/Users/Mamoru/Documents ~/Documents;
 ln -s -f /cygdrive/c/Users/Mamoru/Pictures  ~/Pictures;
 ln -s -f /cygdrive/c/Users/Mamoru/Videos    ~/Videos;
 ln -s -f /cygdrive/c/Users/Mamoru/Music     ~/Music;
-ln -s -f /cygdrive/c/Users/Mamoru/Temp      /cygdrive/c/Users/Mamoru/AppData/Local/Temp
+ln -s -f /cygdrive/c/Users/Mamoru/AppData/Local/Temp ~/Temp
 #SVN
 SVN_LOCAL_PATH=/cygdrive/c/Users/Mamoru/Documents/ITF-2
 ln -s -f ${SVN_LOCAL_PATH}                  ~/ITF-2;
