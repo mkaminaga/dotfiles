777f6a0ac6ce26ceaeefff57076a9f3e7f19926d
mkaminaga <mkaminaga2.718@gmail.com>
2017-04-23 21:58:05 +0900

    create_link.sh changed alot

--- create_link.sh
+++ create_link.sh
@@ -2,121 +2,88 @@
 
 ########## Description ##########
 # Script to create link
-# Use collect command form patterns below:
+# Usage:
+# ./create_link.sh <Home> <pc_flag> <terminal_flag>
 #
-# Pattern 1: Cygwin at My PC
-# ./create_link.sh /cygdrive/c/cygwin64/home/Mamoru my_pc cygwin
+# Example:
+# 1) Cygwin at My PC
+# ./create_link.sh C:\\cygwin64\\home\\Mamoru my_pc cygwin
 #
-# Pattern 2: Msys at Laboratry
-# ./create_link.sh /c/msys64/home/Mamoru my_pc msys2
+# 2) Msys at Laboratry
+# ./create_link.sh C:\\msys64\\home\\Mamoru my_pc msys2
 #
-# Pattern 3: Cygwin at Laboratry
-# ./create_link.sh /cygdrive/c/cygwin64/home/mkami lab_pc cygwin
+# 3) Cygwin at Laboratry
+# ./create_link.sh C:\\cygwin64\\home\\mkami lab_pc cygwin
 #
-# Pattern 4: Msys at Laboratry
-# ./create_link.sh /c/msys64/home/mkami lab_pc msys2
+# 4) Msys at Laboratry
+# ./create_link.sh C:\\msys64\\home\\mkami lab_pc msys2
 
 ########## Constants ##########
-# Arg 2 constants, PC
-PC_MINE="my_pc"
-PC_LABS="lab_pc"
+# pc_flag, PC
+CONST_MYPC="my_pc"
+CONST_LABPC="lab_pc"
 
-# Arg 3 constants, Terminal
-TERMINAL_CYGWIN="cygwin"
-TERMINAL_MSYS2="msys2"
+# terminal_flag, Terminal
+CONST_CYGWIN="cygwin"
+CONST_MSYS2="msys2"
+
 ########## Functions ##########
-
-# Common dotfiles
-function dotfile_links () {
+#### Common ####
+function common_links_for_all () {
   # Dotfiles (Common)
-  ln -s -f ${1}/dotfiles/.ctags         ${1}/.ctags
-  ln -s -f ${1}/dotfiles/.git           ${1}/.git
-  ln -s -f ${1}/dotfiles/.gitattributes ${1}/.gitattributes
-  ln -s -f ${1}/dotfiles/.gitignore     ${1}/.gitignore
-  ln -s -f ${1}/dotfiles/.gvimrc        ${1}/.gvimrc
-  ln -s -f ${1}/dotfiles/.minttyrc      ${1}/.minttyrc
-  ln -s -f ${1}/dotfiles/.my_vim        ${1}/.my_vim
-  ln -s -f ${1}/dotfiles/.vimrc         ${1}/.vimrc
+  \cmd /c "mklink /H ${1}\\.ctags         ${1}\\dotfiles\\.ctags"
+  \cmd /c "mklink /H ${1}\\.git           ${1}\\dotfiles\\.git"
+  \cmd /c "mklink /H ${1}\\.gitattributes ${1}\\dotfiles\\.gitattributes"
+  \cmd /c "mklink /H ${1}\\.gitignore     ${1}\\dotfiles\\.gitignore"
+  \cmd /c "mklink /H ${1}\\.gvimrc        ${1}\\dotfiles\\.gvimrc"
+  \cmd /c "mklink /H ${1}\\.minttyrc      ${1}\\dotfiles\\.minttyrc"
+  \cmd /c "mklink /H ${1}\\.my_vim        ${1}\\dotfiles\\.my_vim"
+  \cmd /c "mklink /H ${1}\\.vimrc         ${1}\\dotfiles\\.vimrc"
 }
 
-# Cygwin for my PC
-function my_PC_cygwin () {
-  # Dotfiles (Local)
-  ln -s -f ${1}/dotfiles/.bashrc_cygwin       ${1}/.bashrc
-  ln -s -f ${1}/dotfiles/.bash_profile_cygwin ${1}/.bash_profile
-
+#### My PC ####
+## Common links
+function common_links_for_mypc () {
   # Windows shortcuts
-  ln -s -f /cygdrive/c/Users/Mamoru/Downloads ${1}/Downloads
-  ln -s -f /cygdrive/c/Users/Mamoru/Desktop   ${1}/Desktop
-  ln -s -f /cygdrive/c/Users/Mamoru/Documents ${1}/Documents
-  ln -s -f /cygdrive/c/Users/Mamoru/Pictures  ${1}/Pictures
-  ln -s -f /cygdrive/c/Users/Mamoru/Videos    ${1}/Videos
-  ln -s -f /cygdrive/c/Users/Mamoru/Music     ${1}/Music
-
-  # Cache
-  ln -s -f /cygdrive/c/Users/Mamoru/AppData/Local/Temp ${1}/Temp
-
-  #SVN
-  SVN_LOCAL_PATH=/cygdrive/c/Users/Mamoru/Documents/ITF-2
-  ln -s -f ${SVN_LOCAL_PATH}          ${1}/ITF-2
-  ln -s -f ${SVN_LOCAL_PATH}/"C&Dh"   ${1}/CDH
-  ln -s -f ${SVN_LOCAL_PATH}/"地上局" ${1}/groundStation
-
-  # University
-  ln -s -f /cygdrive/c/Users/Mamoru/Documents/ut_lectures/Lab ${1}/Lab
-
-  # project
-  ln -s -f /cygdrive/c/Users/Mamoru/Documents/YUI ${1}/yui
+  \cmd /c "mklink /J ${1}\\Downloads C:\\cygdrive\\c\\Users\\Mamoru\\Downloads"
+  \cmd /c "mklink /J ${1}\\Desktop   C:\\cygdrive\\c\\Users\\Mamoru\\Desktop"
+  \cmd /c "mklink /J ${1}\\Documents C:\\cygdrive\\c\\Users\\Mamoru\\Documents"
+  \cmd /c "mklink /J ${1}\\Pictures  C:\\cygdrive\\c\\Users\\Mamoru\\Pictures"
+  \cmd /c "mklink /J ${1}\\Videos    C:\\cygdrive\\c\\Users\\Mamoru\\Videos"
+  \cmd /c "mklink /J ${1}\\Music     C:\\cygdrive\\c\\Users\\Mamoru\\Music"
+  \cmd /c "mklink /J ${1}\\Temp      C:\\cygdrive\\c\\Users\\Mamoru\\AppData\\Local\\Temp"
 
   # programming
-  ln -s -f /cygdrive/c/projects ${1}/projects
-
-  # gimp 2.0
-  ln -s -f /cygdrive/c/cygwin64/home/Mamoru/.gimp-2.8/palettes ${1}/gimpPalettes
-
-  # vim include completion dir setting
-  ln -s -f /cygdrive/c/"Program Files (x86)"/"Microsoft DirectX SDK (June 2010)"/Include ${1}/.vim/dir/directx
-}
-
-# MSYS2 for my PC
-function my_PC_msys2 () {
-  # Dotfiles (Local)
-  ln -s -f ${1}/dotfiles/.bashrc_msys2       ${1}/.bashrc
-  ln -s -f ${1}/dotfiles/.bash_profile_msys2 ${1}/.bash_profile
-
-  # Windows shortcuts
-  ln -s -f /c/Users/Mamoru/Downloads ${1}/Downloads
-  ln -s -f /c/Users/Mamoru/Desktop   ${1}/Desktop
-  ln -s -f /c/Users/Mamoru/Documents ${1}/Documents
-  ln -s -f /c/Users/Mamoru/Pictures  ${1}/Pictures
-  ln -s -f /c/Users/Mamoru/Videos    ${1}/Videos
-  ln -s -f /c/Users/Mamoru/Music     ${1}/Music
-
-  # Cache
-  ln -s -f /c/Users/Mamoru/AppData/Local/Temp ${1}/Temp
+  \cmd /c "mklink /J ${1}\\projects C:\\cygdrive\\c\\projects"
 
   #SVN
-  SVN_LOCAL_PATH=/c/Users/Mamoru/Documents/ITF-2
-  ln -s -f ${SVN_LOCAL_PATH}          ${1}/ITF-2
-  ln -s -f ${SVN_LOCAL_PATH}/"C&Dh"   ${1}/CDH
-  ln -s -f ${SVN_LOCAL_PATH}/"地上局" ${1}/groundStation
+  SVN_PATH=C:\\cygdrive\\c\\Users\\Mamoru\\Documents\\ITF-2
+  \cmd /c "mklink /J ${1}\\ITF-2         ${SVN_PATH}"
+  \cmd /c "mklink /J ${1}\\CDH           ${SVN_PATH}/\"C&Dh\""
+  \cmd /c "mklink /J ${1}\\groundStation ${SVN_PATH}/地上局"
 
   # University
-  ln -s -f /c/Users/Mamoru/Documents/ut_lectures/Lab ${1}/Lab
+  \cmd /c "mklink /J ${1}\\Lab C:\\cygdrive\\c\\Users\\Mamoru\\Documents\\ut_lectures\\Lab"
 
   # project
-  ln -s -f /c/Users/Mamoru/Documents/YUI ${1}/yui
-
-  # programming
-  ln -s -f /c/projects ${1}/projects
-
-  # gimp 2.0
-  ln -s -f /c/cygwin64/home/Mamoru/.gimp-2.8/palettes ${1}/gimpPalettes
-
-  # vim include completion dir setting
-  ln -s -f /c/"Program Files (x86)"/"Microsoft DirectX SDK (June 2010)"/Include  ${1}/.vim/dir/directx
+  \cmd /c "mklink /J ${1}\\yui C:\\cygdrive\\c\\Users\\Mamoru\\Documents\\YUI"
 }
 
+# Cygwin links
+function cygwin_links_for_mypc () {
+  # Dotfiles (Local)
+  \cmd /c "mklink /H ${1}\\.bashrc       ${1}\\dotfiles\\.bashrc_cygwin"
+  \cmd /c "mklink /H ${1}\\.bash_profile ${1}\\dotfiles\\.bash_profile_cygwin"
+}
+
+# MSYS2 links
+function msys_links_for_mypc () {
+  # Dotfiles (Local)
+  \cmd /c "mklink /H ${1}\\.bashrc       ${1}\\dotfiles\\.bashrc_msys2"
+  \cmd /c "mklink /H ${1}\\.bash_profile ${1}\\dotfiles\\.bash_profile_msys2"
+}
+
+#### Lab PC ####
 # Cygwin for Lab PC
 function lab_PC_cygwin () {
   # Dotfiles (Local)
@@ -154,66 +121,56 @@ function lab_PC_msys2 () {
 }
 ########## Calls ##########
 
-# Argument check
+# Argument length check
 if [ $# -ne 3 ]; then
-  echo "Arg error"
-  # Arg 1 check
-  echo "Arg 1!"
-  echo "HOME"
+  echo "Format error"
+  echo "Usage:"
+  echo "./create_link.sh <Home> <pc_flag> <terminal_flag>"
+  exit 1
+fi
 
-  # Arg 2 check
-  echo "Arg 2!"
-  echo "${PC_MINE}: Own PC"
-  echo "${PC_LABS}: Lab PC"
+# Argument 2 check
+if [ [ $2 != ${CONST_MYPC} ] && [ $2 != ${CONST_LABPC} ]]; then
+  echo "Argument 2 error"
+  echo "${CONST_MYPC}"
+  echo "${CONST_LABPC}"
+  exit 1
+fi
 
-  # Arg 3 check
-  echo "Arg 3!"
-  echo "${TERMINAL_CYGWIN}: Cygwin terminal"
-  echo "${TERMINAL_MSYS2}: MSYS2 terminal"
-
-  # Error exit
+# Argument 3 check
+if [ [ $3 != ${CONST_CYGWIN} ] && [ $3 != ${CONST_MSYS2} ]]; then
+  echo "Argument 3 error"
+  echo "${CONST_CYGWIN}"
+  echo "${CONST_MSYS2}"
   exit 1
 fi
 
 # Home directory set
-HOME=$1
+CONST_HOME=$1
 
 # Common link generation
-dotfile_links ${HOME}
+common_links_for_all ${CONST_HOME}
 
 # Separated Link generation
-if [ $2 == ${PC_MINE} ]; then
-
+if [ $2 == ${CONST_MYPC} ]; then
   # For My PC
-  if [ $3 == ${TERMINAL_CYGWIN} ]; then
-    my_PC_cygwin ${HOME}
-  elif [ $3 == ${TERMINAL_MSYS2} ]; then
-    my_PC_msys2 ${HOME}
-  else
+  common_links_for_mypc ${CONST_HOME}
 
-    # Erro Exit
-    echo "Arg3 error"
-    exit 1
+  if [ $3 == ${CONST_CYGWIN} ]; then
+    cygwin_links_for_mypc ${CONST_HOME}
+
+  elif [ $3 == ${CONST_MSYS2} ]; then
+    msys_links_for_mypc ${CONST_HOME}
+
   fi
-elif [ $2 == ${PC_LABS} ]; then
-
+elif [ $2 == ${CONST_LABPC} ]; then
   # For Lab PC
-  if [ $3 == ${TERMINAL_CYGWIN} ]; then
-    lab_PC_cygwin ${HOME}
-  elif [ $3 == ${TERMINAL_MSYS2} ]; then
-    lab_PC_msys2 ${HOME}
-  else
 
-    # Erro Exit
-    echo "Arg3 error"
-    exit 1
+  if [ $3 == ${CONST_CYGWIN} ]; then
+    lab_PC_cygwin ${CONST_HOME}
+
+  elif [ $3 == ${CONST_MSYS2} ]; then
+    lab_PC_msys2 ${CONST_HOME}
+
   fi
-else
-
-  # Erro Exit
-  echo "Arg2 error"
-  exit 1
 fi
-
-# End of the script
-exit 1
